f7712c789a5c33941dfacc4886ce687279e31687
Henrique Marcuzzo <henrique.souza.m06@gmail.com>
2021-03-31 00:07:28 -0300

    Terminando a lógica interna do Pet e seus métodos públicos

--- App/Pet/Pet.cs
+++ App/Pet/Pet.cs
@@ -10,40 +10,136 @@ public class Pet : StaticBody2D {
 	private DateTime last_login;
 
 	// ATRIBUTOS
-	private int happy;
-	private int hunger;
-	private int health;
+	private int happy; //
+	private int hunger; //
+	private int health; //
 
 	// STATUS
-	private int tired;
-	private int dirty;
-	private int sad;
-	private int sleeping;
+	private int tired; // tired
+	private int dirty; // tempo
+	private int sad; // 
+	private int sleeping; //
 
-	private bool normal;
-	private bool sick;
-	private bool dead;
-	private bool light;
+	private bool normal; // ou está normal ou está doente
+	private bool sick; //
+	private bool dead; // vida chega em 0 morre
+	private bool light; // apaga ou desliga
 
 	// AÇÕES
 	public void action_feed(int pid) {
-
+		int random_feed = random_num(1, 11);
+		
+		hunger += random_feed;
+		set_dirty(pid, hunger);
 	}
 
 	public void action_toilet(int pid) {
+		int random_clean = random_num(1, 51);
+		
+		if (light && !dead) {
+			dirty += random_clean;	
+		}
 
+		set_dirty(pid, dirty);
 	}
 
 	public void action_play(int pid) {
+		
+		if (light && !dead) {
+			int random_dirty = random_num(1, 11);
+			int random_tired = random_num(1, 16);
+			int random_sleeping = random_num(1, 16);
+			int random_hunger = random_num(1, 21);
+			int random_happy = random_num(1, 11);
 
+			if (sick) {
+				random_tired *= 2;
+				random_sleeping *= 2;
+				random_happy /= 2;
+			}	
+
+			if (hunger > GREAT) {
+				health -= random_num(1, 6);
+
+				if (health < 0) {
+					health = 0;
+					set_dead(pid, true);
+					set_health(pid, health);
+				}
+			}
+
+			dirty -= random_dirty;
+			tired -= random_tired;
+			sleeping -= random_sleeping;
+			hunger -= random_hunger;
+			happy += random_happy;
+
+			if (dirty < 0) {
+				dirty = 0;
+			}
+			if (tired < 0) {
+				tired = 0;
+			}
+			if (sleeping < 0) {
+				sleeping = 0;
+			}
+			if (hunger < 0) {
+				hunger = 0;
+			}
+			if (happy > 100) {
+				happy = 100;
+			}
+
+			set_dirty(pid, dirty);
+			set_tired(pid, tired);
+			set_sleeping(pid, sleeping);
+			set_hunger(pid, hunger);
+			set_happy(pid, happy);
+			set_sad(pid, (100 - happy));
+		}
+
+		
 	}
 
 	public void action_cure(int pid) {
+		int random_percent = random_num(1, 101);
 
+		if (light) {
+			if (random_percent <= 25 && sick) {
+				sick = false;
+				normal = true;
+				health += random_num(1, 6);
+
+				if (health > 100) {
+					health = 100;
+				}
+
+				set_health(pid, health);
+				set_sick(pid, sick);
+				set_dead(pid, normal);
+			}
+			else {
+				health -= random_num(1, 6);
+
+				if (health < 0) {
+					health = 0;
+					set_dead(pid, true);
+				}
+
+				set_health(pid, health);
+			}
+		}
 	}
 
 	public void action_light(int pid) {
+		if(light) {
+			light = false;
+		}
+		else {
+			light = true;
+		}
 
+		set_light(pid, light);
 	}
 
 
@@ -180,7 +276,7 @@ public class Pet : StaticBody2D {
 	// USEFUL METHODS
 	private int random_num(int min, int max) {
 		Random random_num = new Random();
-		return random_num.Next(1, 1001);
+		return random_num.Next(min, max);
 	}
 
 	// UPDATES
@@ -189,7 +285,7 @@ public class Pet : StaticBody2D {
 		int value = happy;
 
 		if (light) {
-			int percent  = random_num(1, 1001);
+			int percent  = random_num(1, 10001);
 			float increase = 0; 
 
 			if (percent <= 100) {
@@ -207,13 +303,44 @@ public class Pet : StaticBody2D {
 			value = 100;
 		}
 
-		set_happy(pid, value);
-		set_sad(pid, (100 - value));
+		happy = value;
+		set_happy(pid, happy);
+		set_sad(pid, (100 - happy));
 	}
+
+
+	private void update_sad(int pid) {
+		// fome, saude, cansaço, tristeza, sono
+		int value = sad;
+
+		if (light) {
+			int percent  = random_num(1, 10001);
+			float increase = 0; 
+
+			if (percent <= 100) {
+				increase = (hunger * 310 + health * 150 + (100 - tired) * 150 + (100 - happy) * 80 + (100 - sleeping) * 310) / 10000;
+			}
+
+			if(sick) {
+				increase *= 2;
+			}
+
+			value += (int)increase;
+		}
+
+		if (value > 100) {
+			value = 100;
+		}
+
+		sad = value;
+		set_sad(pid, sad);
+		set_happy(pid, (100 - sad));
+	}
+
 
 	private void update_hunger(int pid) {
 		// tempo, luz
-		int percent  = random_num(1, 1001);
+		int percent  = random_num(1, 10001);
 		int value = hunger;
 
 		if (!light) {
@@ -235,6 +362,7 @@ public class Pet : StaticBody2D {
 		set_hunger(pid, hunger);
 
 	}
+
 
 	private void update_health(int pid) {
 		// hunger, tired, sad, sleeping
@@ -285,7 +413,7 @@ public class Pet : StaticBody2D {
 			increment += 1;
 		}
 
-		int percent  = random_num(1, 1001);
+		int percent  = random_num(1, 10001);
 
 		if (increment <= 0 && percent <= 92) {
 			value += increment;
@@ -298,7 +426,7 @@ public class Pet : StaticBody2D {
 			value = 100;
 		}
 
-		if(value < 0) {
+		if(value <= 0) {
 			value = 0;
 			dead = true;
 			set_dead(pid, dead);
@@ -306,6 +434,165 @@ public class Pet : StaticBody2D {
 
 		health = value;
 		set_health(pid, health);
+	}
+
+	
+	private void update_tired(int pid) {
+		int percent  = random_num(1, 10001);
+
+		if(percent <= 34  && light) {
+			if (sick) {
+				tired -= 1 * 2;
+			}
+			else {
+				tired -= 1;
+			}
+		}
+
+		if(!light && percent <= 102) {
+			if (sick) {
+				tired += 1;
+			}
+			else {
+				tired += 1 * 2;
+			}
+		}
+
+		if (tired < 0) {
+			tired = 0;
+		}
+
+		if (tired > 100) {
+			tired = 100;
+		}
+
+		set_tired(pid, tired);
+	}
+
+	private void update_dirty(int pid) {
+		int percent  = random_num(1, 10001);
+
+		if(percent <= 46 && light) {
+			dirty -= 1;
+		}
+
+		set_dirty(pid, dirty);
+	}
+
+
+	private void update_sleeping(int pid) {
+		int percent  = random_num(1, 10001);
+
+		if(light && percent <= 34) {
+			if (sick) {
+				sleeping -= 1 * 2;
+			}
+			else {
+				sleeping -= 1;
+			}
+		}
+
+		if(!light && percent <= 102) {
+			if (sick) {
+				sleeping += 1;
+			}
+			else {
+				sleeping += 1 * 2;
+			}
+		}
+
+		if (sleeping < 0) {
+			sleeping = 0;
+		}
+
+		if (sleeping > 100) {
+			sleeping = 100;
+		}
+
+		set_sleeping(pid, sleeping);
+	}
+
+
+	private void update_sick(int pid) {
+		int percent  = random_num(1, 10001);
+
+		float sad_percent = sad + 1;
+		float hunger_percent = hunger + 1;
+		float tired_percent = tired + 1;
+		float sleeping_percent = sleeping + 1;
+		float dirty_percent = sad + 1;
+		
+		int sick_percent = 0;
+
+		if (sad < TERRIBLE) {
+			sick_percent += 3;
+		}
+		else if (sad >= TERRIBLE && sad <= GREAT ) {
+			sick_percent += 5;
+		}
+		else {
+			sick_percent += 10;
+		}
+
+		if (hunger < TERRIBLE) {
+			sick_percent += 10;
+		}
+		else if (hunger >= TERRIBLE && hunger <= GREAT ) {
+			sick_percent += 5;
+		}
+		else {
+			sick_percent += 3;
+		}
+
+		if (tired < TERRIBLE) {
+			sick_percent += 10;
+		}
+		else if (tired >= TERRIBLE && tired <= GREAT ) {
+			sick_percent += 5;
+		}
+		else {
+			sick_percent += 3;
+		}
+
+		if (sleeping < TERRIBLE) {
+			sick_percent += 10;
+		}
+		else if (sleeping >= TERRIBLE && sleeping <= GREAT ) {
+			sick_percent += 5;
+		}
+		else {
+			sick_percent += 3;
+		}
+
+		if (dirty < TERRIBLE) {
+			sick_percent += 10;
+		}
+		else if (dirty >= TERRIBLE && dirty <= GREAT ) {
+			sick_percent += 5;
+		}
+		else {
+			sick_percent += 3;
+		}
+
+		// SICK COMPUTATION
+		if (normal && percent <= sick_percent) {
+			normal = false;
+			sick = true;
+			health -= 5;
+		}
+		else if (sick && percent <= sick_percent * 2) {
+			health -= 10;
+		}
+
+		if(health <= 0) {
+			health = 0;
+			dead = true;
+			set_dead(pid, dead);
+		}
+
+		set_health(pid, health);
+		set_normal(pid, normal);
+		set_sick(pid, sick);
 	}
 
 
@@ -318,20 +605,27 @@ public class Pet : StaticBody2D {
 			ticks = 1;
 		}
 		GD.Print(ticks);
-
+		GD.Print("Health: ", health);
+		
 		int k = 0;
 		while (k < ticks) {
-			update_happy(pid);
-			update_hunger(pid);
-			update_health(pid);
+			if(!dead) {
+				update_happy(pid);
+				update_hunger(pid);
+				update_health(pid);
+				update_tired(pid);
+				update_dirty(pid);
+				update_sad(pid);
+				update_sleeping(pid);
+				update_sick(pid);
+			}
 
 			k++;
 		}
 
 		set_last_login(pid, time_now);
 
-		GD.Print("Happy: ", happy, "\nSad: ", sad);
-		GD.Print("Hunger: ", hunger);
+		
 	}
 
 
@@ -342,8 +636,8 @@ public class Pet : StaticBody2D {
 		sad = 50;
 		hunger = 40;
 		health = 80;
-		tired = 20;
-		sleeping = 20;
+		tired = 40;
+		sleeping = 40;
 		dirty = 80;
 
 		normal = true;
